17140243f535983b367342bffd847a62583a67af
Jie Chen <jie.chen.2020@marshall.usc.edu>
2020-07-18 18:28:11 -0700

    Update GoogleMapAPI.py

--- GoogleMapAPI.py
+++ GoogleMapAPI.py
@@ -3,7 +3,7 @@ import requests
 
 #Google MAP API call
 # params = {
-# 'key': 'AIzaSyAE-tQ0-K_j78fDO-IFXHXbKso35013mcc',
+# 'key': 'xxxxxxxxxxxxxxxxxxxxxxxxx',
 # 'origins': '4810 Viro Rd, La Canada, CA, USA',
 # 'destinations': '1941 Lombardy, La Canada, CA, USA'
 # }
@@ -17,7 +17,7 @@ import requests
 
 #Google Place API call
 params = {
-'key': 'AIzaSyAE-tQ0-K_j78fDO-IFXHXbKso35013mcc',
+'key': 'xxxxxxxxxxxxxxxxxxxxxxxxxxc',
 'q':'Los Angeles, CA, USA'
 }
 # url ='https://www.google.com/maps/embed/v1/place/json?'
